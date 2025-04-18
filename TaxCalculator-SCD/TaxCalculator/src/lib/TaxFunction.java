a2d59e55478ff68e10195ab7bb6f4427ffbe1c53
Born2Simp <149867359+Born2Simp@users.noreply.github.com>
2025-04-18 23:44:54 +0700

    Update TaxFunction.java
    memisahkan method calculateTax() menjadi sub-method:

--- TaxCalculator-SCD/TaxCalculator/src/lib/TaxFunction.java
+++ TaxCalculator-SCD/TaxCalculator/src/lib/TaxFunction.java
@@ -15,30 +15,11 @@ public class TaxFunction {
 	 */
 	
 	
-	public static int calculateTax(int monthlySalary, int otherMonthlyIncome, int numberOfMonthWorking, int deductible, boolean isMarried, int numberOfChildren) {
-		
-		int tax = 0;
-		
-		if (numberOfMonthWorking > 12) {
-			System.err.println("More than 12 month working per year");
-		}
-		
-		if (numberOfChildren > 3) {
-			numberOfChildren = 3;
-		}
-		
-		if (isMarried) {
-			tax = (int) Math.round(0.05 * (((monthlySalary + otherMonthlyIncome) * numberOfMonthWorking) - deductible - (54000000 + 4500000 + (numberOfChildren * 1500000))));
-		}else {
-			tax = (int) Math.round(0.05 * (((monthlySalary + otherMonthlyIncome) * numberOfMonthWorking) - deductible - 54000000));
-		}
-		
-		if (tax < 0) {
-			return 0;
-		}else {
-			return tax;
-		}
-			 
-	}
+	private static int calculateNonTaxableIncome(boolean isMarried, int numberOfChildren) {
+    int nonTaxable = 54000000;
+    if (isMarried) nonTaxable += 4500000;
+    nonTaxable += Math.min(numberOfChildren, 3) * 1500000;
+    return nonTaxable;
+}
 	
 }
